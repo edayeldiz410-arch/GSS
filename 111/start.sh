8ae3c01e6b4e9f6d71d46da614a3e97fb4fafbd8
edayeldiz410-arch <edayeldiz410@gmail.com>
2025-12-04 15:39:10 +0100

    Add start.sh script for Django application setup

---
+++ 111/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Navigate to the Django project directory
+cd school
+
+# Run database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the application using Gunicorn
+gunicorn school.wsgi --bind 0.0.0.0:$PORT
